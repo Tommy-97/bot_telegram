51850edd2bc661d71562aacc66dc9fc5d9ff846a
tommy <tommyk9797@gmail.com>
2024-05-11 14:16:11 +0300

    Доставлено без сущности системы безопасности

--- config.py
+++ config.py
@@ -1,14 +1,12 @@
-#config.py
+# config.py
 from aiogram import Bot, types
 
 
 async def send_message_to_user(chat_id, text):
-    bot = Bot(token="6969199222:AAFCiKI3cZbVnrhDhsyYZDpW3LBzFW36Eiw")  
+    bot = Bot(token="KEY")
     await bot.send_message(chat_id, text)
 
 
-
-API_TOKEN = '6969199222:AAFCiKI3cZbVnrhDhsyYZDpW3LBzFW36Eiw'
+API_TOKEN = 'KEY'
 
 SQLALCHEMY_URL = "sqlite:///F:/SQLete/Auto_base.db"
-
